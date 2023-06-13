806c8e3c18ed5fd9a8ba931ef91a81f873df8ae8
Maryna Petrenko <marinbonibon@gmail.com>
2023-06-13 21:12:32 +0200

    feat: create fresh.txt

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,41 @@
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+import { open, close, writeFile } from 'node:fs';
+
+const createFile = async (filePath, content) => {
+  writeFile(filePath, content, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Text file created successfully!');
+  });
+}
+
 const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, 'files', 'fresh.txt');
+  const content = 'I am fresh and young';
+  const errorMessage = 'FS operation failed';
+
+  open(filePath, 'wx', (err, fd) => {
+    if (err) {
+      if (err.code === 'EEXIST') {
+        throw new Error(errorMessage);
+      }
+
+      throw err;
+    }
+
+    try {
+      createFile(filePath, content);
+    } finally {
+      close(fd, (err) => {
+        if (err) throw err;
+      });
+    }
+  });
 };
 
 await create();
