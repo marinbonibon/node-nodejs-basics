1bf36639012eb88b2ad89b398fc35dc81a93c804
Maryna Petrenko <marinbonibon@gmail.com>
2023-06-14 23:32:24 +0200

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,35 @@
+import { close, open } from 'node:fs';
+import { __dirname, errorMessage } from './constants.js';
+import { join } from 'node:path';
+import { unlink } from 'node:fs';
+
+const deleteFile = async (filePath) => {
+  unlink(filePath, (err) => {
+    if (err) {
+      console.error(`Error deleting file: ${err}`);
+      return;
+    }
+
+    console.log('File deleted successfully');
+  });
+}
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+
+  open(filePath, 'r', (err, fd) => {
+    if (err) {
+      throw new Error(errorMessage)
+    }
+
+    try {
+      deleteFile(filePath);
+    } finally {
+      close(fd, (err) => {
+        if (err) throw err;
+      });
+    }
+  });
 };
 
 await remove();
