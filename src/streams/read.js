4e116750712deddfe621c1d5819675528ed09bee
Maryna Petrenko <marinbonibon@gmail.com>
2023-06-15 23:38:53 +0200

    feat: implement reading function using stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from 'node:fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+  const stream = createReadStream(filePath)
+  stream.on('data', (data) => {
+    process.stdout.write(data);
+  })
+  stream.on('error', (err) => {
+    console.log(`Error while createReadStream: ${err}`);
+  });
+
 };
 
 await read();
