de2d9ee25831caf815a8bd91704c317db1083b68
Maryna Petrenko <marinbonibon@gmail.com>
2023-06-17 00:05:03 +0200

    feat: implement compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'node:path';
+
 const compress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const sourcePath = join(__dirname, 'files', 'fileToCompress.txt');
+  const destinationPath = join(__dirname, 'files', 'archive.gz');
+  const gzip = createGzip();
+  const sourceFile = createReadStream(sourcePath);
+  const destinationArchive = createWriteStream(destinationPath);
+
+  pipeline(sourceFile, gzip, destinationArchive, (err) => {
+    if (err) {
+      console.error('Error:', err);
+      process.exitCode = 1;
+    }
+  });
 };
 
 await compress();
