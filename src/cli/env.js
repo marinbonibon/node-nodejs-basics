4f9a851e7eccde397b255cba5375a6960fc824ac
Maryna Petrenko <marinbonibon@gmail.com>
2023-06-18 00:09:33 +0200

    feat: implement function that parses environment variables

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = 'RSS_';
+
+  const result = Object.entries(process.env).reduce((acc, [key, val]) => {
+    if (key.startsWith(prefix)) {
+      acc.push(`${key}=${val}`);
+    }
+    return acc;
+  }, []).join(';');
+
+  console.log(result);
 };
 
 parseEnv();
